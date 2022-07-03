885412911912bc812100067da823945d1e0af08c
Surafel-Abera <93444317+Ky-Surafel-Abera@users.noreply.github.com>
2022-07-03 17:55:32 +0000

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,5 @@
 my_list = [1, 2, 3, 4, 5]
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
+        print_list_integer = __import__('0-print_list_integer').print_list_integer
         print("{}".format(my_list[i]))
