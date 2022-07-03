f8f1c27b76923d6d52d9e5d146c3167eccf85b71
Surafel-Abera <93444317+Ky-Surafel-Abera@users.noreply.github.com>
2022-07-03 18:26:33 +0000

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
 def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print_list_integer = __import__('0-print_list_integer').print_list_integer
-        print("{}".format(my_list[i]))
+    for i in range(0,len(my_list)):
+        print("{:d}".format(my_list[i]))
+my_list = [1,2,3,4,5];
+print_list_integer(my_list)
