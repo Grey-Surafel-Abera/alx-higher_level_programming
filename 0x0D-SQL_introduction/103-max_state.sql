b44aa5a782ec455a8448cb596acbaedccda889df
Surafel-Abera <93444317+Ky-Surafel-Abera@users.noreply.github.com>
2022-09-07 20:07:32 +0000

    Update 103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,6 @@
--- Script that displays the max temperature of each state
--- Query to display the max temperature of each state
-import hbtn_0c_0
-SELECT state, MAX(value) AS max_temp
+-- script that displays the max temperature of each state (ordered by State name).
+-- Importing:  mysql -u username -p  databasename  < path/example.sql
+SELECT `state`, MAX(`value`) AS max_temp
 FROM temperatures
-GROUP BY state
-LIMIT 3;
+GROUP BY `state`
+ORDER BY `state` ASC;
